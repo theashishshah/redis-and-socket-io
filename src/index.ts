3046eea6b1a729be2cfaa3dd0bb8b5213a358346
Ashish Shah <eiepixxstuff@gmail.com>
2025-09-11 03:57:23 +0530

    Implement rate limiter using redis

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,45 @@
 import express from "express"
 import axios from "axios"
 import Redis from "ioredis"
+import cookieParser from "cookie-parser"
+import {v4 as uuidv4} from 'uuid'
 
 const PORT = process.env.PORT ?? 8000
 const redis = new Redis.default({ host: "localhost", port: Number(6379) })
 const app = express()
 
 
-// Cached the total number page count of a book
-// implement rate limiter 
+app.use(cookieParser());
+app.use(async (req, res, next) => {
+    let cookie = req.cookies.session_id
+    if (!cookie) {
+        res.cookie("session_id", uuidv4(), { maxAge: 900000, httpOnly: true })
+    }
+    next()
+})
+
+app.use(async (req, res, next) => {
+    const cookie = req.cookies.session_id
+    const key = `rate-limiter-${cookie}`
+    const cached = await redis.get(key)
+
+    console.log(`${cached} value.`);
+    if (cached === null) {
+        await redis.set(key, 0)
+        await redis.expire(key, 30) // key, seconds
+    }
+
+    if (Number(cached) > 10) {
+        return res.status(429).json({
+            messsage: "Too many request, wait for cool down period",
+            success: false
+        })
+    }
+    
+    await redis.incr(key)
+
+    next()
+})
 
 app.get("/books/page-count",  async (req, res) => {
     const key = `total-pages`
